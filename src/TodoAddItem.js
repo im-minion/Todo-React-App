2edaef9bb195aa2dd6d38c20c7cc4f8f5f164a4d
im-minion <vaibhavminiyar@gmail.com>
2020-10-01 20:57:03 +0530

    added the component for insert todo-item

---
+++ src/TodoAddItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+function TodoAddItem() {
+    return (
+    <div>
+    <input type="text" placeholder="Enter Item To add"/>
+    <button>
+        Add
+    </button>
+    </div>
+    );
+}
+
+export default TodoAddItem;
