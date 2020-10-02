08bee4c63b10e4c87f4e4d4041f2697013967dbb
im-minion <vaibhavminiyar@gmail.com>
2020-10-02 16:04:49 +0530

    build

---
+++ docs/precache-manifest.c3929c09d3f0cb0301b565855f438ae5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0e621237844e4da469a08408dfc03ef",
+    "url": "/todo-react-app/index.html"
+  },
+  {
+    "revision": "8f621461d59e0af9b827",
+    "url": "/todo-react-app/static/css/main.3f38d478.chunk.css"
+  },
+  {
+    "revision": "a8467dd0095d09d1afc2",
+    "url": "/todo-react-app/static/js/2.02e9d9fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react-app/static/js/2.02e9d9fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f621461d59e0af9b827",
+    "url": "/todo-react-app/static/js/main.678953a2.chunk.js"
+  },
+  {
+    "revision": "b1b4ee42d0cf3544f98c",
+    "url": "/todo-react-app/static/js/runtime-main.1b9ea20e.js"
+  }
+]);
